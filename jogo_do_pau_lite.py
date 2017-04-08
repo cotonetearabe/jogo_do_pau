cf3eea398c49b3f2c666cc89744d96792e177113
Sebastião Pereira <ex13745@alunos.uevora.pt>
2017-04-08 12:23:43 +0100

    jogo_do_pau_lite(Sem Xoragora)

--- jogo_do_pau_lite.py
+++ jogo_do_pau_lite.py
@@ -5,7 +5,8 @@ Esta versao só precisa de python3/3.+ e de o ficheiro
 
 Ainda nao testado
 '''
-import math, random, os, time
+import random, time
+
 
 def before():
 	q = input('Queres voltar ao menu?(ç/n)')
@@ -22,28 +23,31 @@ def before():
 		before()
 		choice_of_error()
 
+
 def choice_of_error():
 		print("\n\n\n\n\n\n")
-		print(random.choice(["\nf...\n","Bem isto espumou-çe a 1ª...","Bem... isto esta tudo bugado...","Bem... ou és um atrasado de merda ou estas a espera do que?","################\nThe Ban Hammer has spoken...\n################","#####\nThis is an ERROR Message\n :) sorry\n#####","\nERRRORRR BITCHES!!!\n","\nThis is your fault PAY ME!\n","\nGoogle cannot be reach at this moment...\n\nGET BACK IN YOUR CAGE PLEBIAN\n","Hello i am jogo_do_pau\nI am a python game\nplz devolop me my dev sucks :(\n","Nah dude, im way too drunk to play...\n","##############\nWelcome to the matrix\nThey found you neo!\n##############\n\n\n ps: jk we're all in hell\n\n\n"]))
+		print(random.choice(["\nf...\n", "Bem isto espumou-çe a 1ª...", "Bem... isto esta tudo bugado...", "Bem... ou és um atrasado de merda ou estas a espera do que?", "################\nThe Ban Hammer has spoken...\n################", "#####\nThis is an ERROR Message\n :) sorry\n#####", "\nERRRORRR BITCHES!!!\n", "\nThis is your fault PAY ME!\n", "\nGoogle cannot be reach at this moment...\n\nGET BACK IN YOUR CAGE PLEBIAN\n", "Hello i am jogo_do_pau\nI am a python game\nplz devolop me my dev sucks :(\n", "Nah dude,  im way too drunk to play...\n", "##############\nWelcome to the matrix\nThey found you neo!\n##############\n\n\n ps: jk we're all in hell\n\n\n"]))
 		print("\n\n")
+
+
 def start():
 	x = input("Bem vindo ao jogo_do_pau.\n\nSe quiseres o start_the_game insere 0, the_game 1, tracadinho 2 um_vs_zero 3, quem_quer_ser_pau 4, xadrez 5, quinta_feira 6, ate_ao_pau 7: \n\n Bons Paus! cotonete.arabe\n\n")
 	y = eval(x)
-	if (y==0):
+	if (y == 0):
 		start_the_game()
-	elif (y==1):
+	elif (y == 1):
 		the_game()
-	elif (y==2):
+	elif (y == 2):
 		tracadinho()
-	elif (y==3):
+	elif (y == 3):
 		um_vs_zero()
-	elif (y==4):
+	elif (y == 4):
 		quem_quer_ser_pau()
-	elif (y==5):
+	elif (y == 5):
 		xadrez()
-	elif (y==6):
+	elif (y == 6):
 		quinta_feira()
-	elif (y==7):
+	elif (y == 7):
 		ate_ao_pau()
 	else:
 		choice_of_error()
@@ -53,20 +57,20 @@ def start():
 def start_the_game():
 	y = input('Diz ai uma merda qualquer: ')
 	x = eval(y)
-	if (type(x)==int):
+	if (type(x) == int):
 		print('ok se quiseres ir por ai...')
 		y = input("Queres continuar?(ç ou n)\n\n\n")
-		if (y=='ç') or (y=='Ç'):
+		if (y == 'ç') or (y == 'Ç'):
 			print('Pega de mão direita, bebe, volta a encher e bebe outra vez\n\n')
 			before()
-		elif (y=='n') or (y=='N'):
+		elif (y == 'n') or (y == 'N'):
 			print('És um merdas bebe de mão direita, se te recusares, sai da mesa.\n\n')
 		b = input('Bebeste?')
-		if (b=='') or (b == 'Ç'):
+		if (b == '') or (b  ==  'Ç'):
 			o = input('Queres jogar outra vez?')
-			if (o=='ç') or (b=='Ç'):
+			if (o == 'ç') or (b == 'Ç'):
 				start_the_game()
-			elif (o=='n') or (o=='N'):
+			elif (o == 'n') or (o == 'N'):
 				choice_of_error()
 				before()
 			else:
@@ -75,29 +79,29 @@ def start_the_game():
 		else:
 			choice_of_error()
 			start()
-	elif (type(x)==float):
+	elif (type(x) == float):
 		print('Ok peculiar...')
-		rnd = random.randrange(0,99)
+		rnd = random.randrange(0, 99)
 		print('Boas neste jogo caso queiras jogar poderas beber...')
 		print('Só estou a avisar para depois não haver mariqueçes')
 		quest = input('Queres jogar?(ç ou n)')
-		if (quest=='ç') or (quest=='Ç'):
+		if (quest == 'ç') or (quest == 'Ç'):
 			n = float(input('Dá ai um n positivo menor que 100'))
 			print('Ok vamos ver que numero escolheste em relação ao meu?')
-			if (n==rnd) and (n<100):
+			if (n == rnd) and (n < 100):
 				print('Vá foi igual, o teu numero, ', n, 'é igual a, ', rnd, '\nSe for igual bebes tu')
 				before()
-			elif (n>rnd) and (n<100):
-				print('Foi maior, o teu numero, ', n, 'é maior que, ', rnd,'\nSe for maior e nao fores de EI, alguem do curso bebe... se fores de EI nem tens de ir buscar alguem como deve ser')
+			elif (n > rnd) and (n < 100):
+				print('Foi maior, o teu numero, ', n, 'é maior que, ', rnd, '\nSe for maior e nao fores de EI, alguem do curso bebe... se fores de EI nem tens de ir buscar alguem como deve ser')
 				before()
-			elif (n<rnd) and (n<100):
+			elif (n < rnd) and (n < 100):
 				print('Foi menor, o teu numero, ', n, 'é menor que, ', rnd, '\nSe for menor e fores de EI não és obrigado a beber mas é encoranjado')
 				before()
 			else:
 				print('Well... This is awkhard...')
 				choice_of_error()
 				before()
-		elif (quest=='n') or (quest=='N'):
+		elif (quest == 'n') or (quest == 'N'):
 			print('Ok faggit...')
 		else:
 			choice_of_error()
@@ -112,16 +116,16 @@ def start_the_game():
 
 
 def the_game():
-	n = random.randrange(0,13)
+	n = random.randrange(0, 13)
 	if n < 10:
 		print("Oh! shit i lost the the game\n\n\nI should try again....")
 		start()
 	elif n > 10:
 		print("Ok i got the game this time")
 		o = input('Queres outra vez? ')
-		if (o=='ç') or (o=='Ç'):
+		if (o == 'ç') or (o == 'Ç'):
 			the_game()
-		elif (o=='n') or (o=='N'):
+		elif (o == 'n') or (o == 'N'):
 			print('...')
 			before()
 		else:
@@ -132,25 +136,27 @@ def the_game():
 		choice_of_error()
 		before()
 
+
 def tracadinho():
 	pau = input("Pau?(ç/n): ")
 	if pau == 'ç' or pau == 'Ç':
 		for i in range(10):
 			print("Desta vez estou mesmo a rasca...\nVou me pirar de mansinho...\nNão volto aquela tasca, NÃO!...\nNão bebo mais traçadinho..."+("\n"*random.randrange(1,15)))
 			o = input('Queres outra vez? ')
-			if (o=='ç') or (o=='Ç'):
+			if (o == 'ç') or (o == 'Ç'):
 				tracadinho()
-			elif (o=='n') or (o=='N'):
+			elif (o == 'n') or (o == 'N'):
 				print('...')
 				start()
 			else:
 				before()
-	elif (pau=='n') or (pau=='N'):
+	elif (pau == ' n ') or (pau == ' N '):
 		print("És uma desgraça para a UE...\n\n\n")
 		before()
 	else:
 		choice_of_error()
 		before()
+
 
 def um_vs_zero():
 	x = input('Quantos ciclos queres?: \n')
